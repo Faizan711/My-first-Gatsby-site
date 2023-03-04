c76a8f4a4042853030f06792b1c3664d67146e92
Faizan <faizanalam980@gmail.com>
2023-03-04 12:52:08 +0530

    nested the index and about with layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 // Step 1: Import React
 import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>This is according to the Gatsby tutorial</p>
+    </Layout>
   )
 }
 
 // You'll learn about this in the next task, just copy it for now
-export const Head = () => <title>Home Page</title>
+export const Head = () => <title>Home</title>
 
 // Step 3: Export your component
 export default IndexPage
